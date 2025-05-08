d089e697a84d79c385c55c928d3d6cd1790d58e3
UriAcelga <urigomez2002@gmail.com>
2025-05-08 11:12:52 -0300

    añadida pwd a UsuarioModel

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -9,13 +9,14 @@ class UsuarioModel extends Model
     protected $useAutoIncrement = true;
     protected $returnType = 'array';
     protected $useSoftDeletes = false;
-    protected $allowedFields = ['id', 'email'];
+    protected $allowedFields = ['id', 'email', 'pwd'];
     protected $useTimestamps = false;
     protected $createdField = 'created_at';
     protected $updatedField = 'updated_at';
     protected $deletedField = 'deleted_at';
     protected $validationRules = [
         'email' => 'required|valid_email|is_unique[usuario.email]',
+        'pwd' => 'required|min_length[8]|max_length[30]',
     ];
     protected $validationMessages = [
         'email' => [
